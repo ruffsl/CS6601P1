291847ecce675f81cecf5b7685e2b3169eea86bb
Ruffin <roxfoxpox@gmail.com>
2013-10-12 21:29:00 -0400

    working Party Search
    test code:
    G = nx.Graph ()
    G.add_edge('SS', 'SR',weight = 1)
    G.add_edge('SS','SE', weight = 1)
    
    G.add_edge('SR',1, weight = 2)
    G.add_edge('SR',2, weight = 100)
    
    G.add_edge('SE',2, weight = 100)
    G.add_edge('SE',3, weight = 3)
    
    G.add_edge(1,'DE', weight = 2)
    G.add_edge(2,'DE', weight = 100)
    G.add_edge(2,'DR', weight = 100)
    G.add_edge(3,'DR', weight = 3)
    
    G.add_edge('DR','SD', weight = 1)
    G.add_edge('DE','SD', weight = 1)
     
    nx.draw(G)
    path = party_dijkstra(G,source='SS',target='SD',cutoff=None,weight='weight')
    path

--- Code/Python/ccrp_tools.py
+++ Code/Python/ccrp_tools.py
@@ -1,10 +1,8 @@
-__all__ = ['shortest_djikstra']
-
 import heapq
 import networkx as nx
 from networkx.utils import generate_unique_node
 
-def shortest_dijkstra(G,source,target=None,cutoff=None,weight='weight',evac_dest = None, resc_dest = None, evac_src = None):
+def party_dijkstra(G,source,target=None,cutoff=None,weight='weight'):
     """Compute shortest paths and lengths in a weighted graph G.
 
     Uses Dijkstra's algorithm for shortest paths.
@@ -28,35 +26,6 @@ def shortest_dijkstra(G,source,target=None,cutoff=None,weight='weight',evac_dest
        Returns a tuple of two dictionaries keyed by node.
        The first dictionary stores distance from the source.
        The second stores the path from the source to that node.
-
-
-    Examples
-    --------
-    >>> G=nx.path_graph(5)
-    >>> length,path=nx.single_source_dijkstra(G,0)
-    >>> print(length[4])
-    4
-    >>> print(length)
-    {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
-    >>> path[4]
-    [0, 1, 2, 3, 4]
-
-    Notes
-    ---------
-    Edge weight attributes must be numerical.
-    Distances are calculated as sums of weighted edges traversed.
-
-    Based on the Python cookbook recipe (119466) at
-    http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/119466
-
-    This algorithm is not guaranteed to work if edge weights
-    are negative or are floating point numbers
-    (overflows and roundoff errors can cause problems).
-
-    See Also
-    --------
-    single_source_dijkstra_path()
-    single_source_dijkstra_path_length()
     """
     if source==target:
         return ({source:0}, {source:[source]})
@@ -67,50 +36,20 @@ def shortest_dijkstra(G,source,target=None,cutoff=None,weight='weight',evac_dest
     heapq.heappush(fringe,(0,source))
     while fringe:
         (d,v)=heapq.heappop(fringe)
+        #print 'd,v: ',d,',',v
         if v in dist:
             continue # already searched this node.
         dist[v] = d
-        party = 1
-        if paths.has_key('SR'):
-            party = 1
-        if paths.has_key('SE'):
-            party = 0
-        print "Length of Paths", len(paths)
+        if v == target:
+            break
         
-        items = paths.items()
-        print "Paths", paths
-        #if items[len(items)-1][0] in resc_dest:
-        #    print 'True'
-        if paths.has_key('SD'):
-            print 'True',paths['SD']
-            if paths['SD'][1] in evac_src:
-                if paths['SD'][len(paths['SD'])-2] in evac_dest:
-                    if v == target:
-                        break
-            else:
-                if paths['SD'][len(paths['SD'])-2] in resc_dest:
-                    if v == target:
-                        break
-            
-        print items[len(items)-1][0]
-        #if len (paths)>0:
-        #    party = paths[1].lstrip('S')
-        #    print 'Party' : party
-        #if v == target:
-        #    break
-        #for ignore,w,edgedata in G.edges_iter(v,data=True):
-        #is about 30% slower than the following
-        if G.is_multigraph():
-            edata=[]
-            for w,keydata in G[v].items():
-                minweight=min((dd.get(weight,1)
-                               for k,dd in keydata.items()))
-                edata.append((w,{weight:minweight}))
-        else:
-            edata=iter(G[v].items())
-
+        edata=iter(G[v].items())
         for w,edgedata in edata:
+            #if v == 2:
+                #print 'bottle neck'
+                #print 'w,edgedata: ', w, ',', edgedata
             vw_dist = dist[v] + edgedata.get(weight,1)
+                
             if cutoff is not None:
                 if vw_dist>cutoff:
                     continue
@@ -119,7 +58,22 @@ def shortest_dijkstra(G,source,target=None,cutoff=None,weight='weight',evac_dest
                     raise ValueError('Contradictory paths found:',
                                      'negative weights?')
             elif w not in seen or vw_dist < seen[w]:
-                seen[w] = vw_dist
-                heapq.heappush(fringe,(vw_dist,w))
-                paths[w] = paths[v]+[w]
-    return (dist,paths)
+                if (str(w)[0] == 'D'):
+                    S = paths[v][1].lstrip('S')
+                    D = str(w).lstrip('D')
+                    #print 'S,D: ', S, D
+                    if S == D:
+                        #print 'Yha!!'
+                        seen[w] = vw_dist
+                        heapq.heappush(fringe,(vw_dist,w))
+                        paths[w] = paths[v]+[w]
+                else:
+                    seen[w] = vw_dist
+                    heapq.heappush(fringe,(vw_dist,w))
+                    paths[w] = paths[v]+[w]
+        #print 'fringe: ', fringe
+        
+    try:
+        return paths[target]
+    except KeyError:
+        raise nx.NetworkXNoPath("node %s not reachable from %s"%(source,target))
